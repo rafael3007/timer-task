3c0e25bcdce8bf229661f581fa0d49964ab375e8
rafael3007 <rafael.brito.1422@gmail.com>
2024-09-02 22:18:14 -0300

    validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
 import {
     CountdownContainer,
@@ -8,18 +11,33 @@ import {
     Separator,
     StartCountdownButton,
     TaskInput,
-} from './styles'
-import { useForm } from "react-hook-form";
+} from './styles';
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa!"),
+    minutesAmount: zod.number()
+        .min(5, "O ciclo precisa ser de no mínimo 5 minutos!")
+        .max(60, "O ciclo precisa ser de no máximo 60 minutos!"),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 
 export function Home() {
 
     const {
         register,
         handleSubmit,
-        watch
-    } = useForm()
+        watch,
+    } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    })
 
-    function handleCreateNewCycle(data: any) {
+    function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data)
     }
 
